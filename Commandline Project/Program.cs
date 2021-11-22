df8283f0e7bb8caddb1d24f4dd75ba06e100883c
GeorgeFritz8 <30572@ma-web.nl>
2021-11-22 15:35:31 +0100

    2nd commit

--- Commandline Project/Program.cs
+++ Commandline Project/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Commandline_Project
 {
     class Program
+
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReviewList database = new ReviewList();
+
+            database.addReview(new Review(5, "Good shit"));
+            database.addReview(new Review(1, "Bad shit"));
+            database.addReview(new Review(3, "Medium shit"));
+
+            foreach (Review review in database.GetReviews())
+            {
+                Console.WriteLine("Reviw is:" + " " + review.Stars + "; " + review.Message);
+            }
+        }
+    }
+    public class ReviewList
+    {
+        private List<Review> _ReviewDatabase;
+
+        public ReviewList()
+        {
+            _ReviewDatabase = new List<Review>();
+        }
+
+        public void addReview(Review ReviewToAdd)
+        { 
+            _ReviewDatabase.Add(ReviewToAdd);
+        }
+        
+        public List<Review> GetReviews()
+        {
+            return _ReviewDatabase;
         }
     }
 }
---
+++ Commandline Project/Review.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Commandline_Project
+{
+    public class Review
+    {
+        public int Stars;
+        public string Message;
+        public Review(int Stars, string Message)
+        {
+            this.Stars = Stars;
+            this.Message = Message;
+        }
+    }
+}
